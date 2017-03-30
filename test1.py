e76e074fbc2e6e79d93434f24611e406d3a6479c
garagehacks <morystark@gmail.com>
2017-03-30 17:06:50 +0430

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+def search_spider(max_pages):
+    page = 1
+    while page < max_pages :
+        url = "http://pop-music.ir/?blogs=" + str(page) + "%2C5&s=%D9%81%D8%B1%D8%B2%D8%A7%D8%AF+%D9%81%D8%B1%D8%B2%DB%8C%D9%86"
+        source_code=requests.get(url)
+        make_text = source_code.text
+        soup = BeautifulSoup(make_text)
+        for link in soup.findAll("a",{"rel" : "bookmark"}):
+            href =link.get("href")
+            print(href)
+
+
+search_spider(2)
+
+
+
+
