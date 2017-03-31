d9095554d14b28caddd70f4ca71049dd2672f48e
garagehacks <morystark@gmail.com>
2017-03-31 17:15:24 +0430

    Create nex1-music-mcrawler.py

---
+++ nex1-music-mcrawler.py
@@ -0,0 +1,24 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+def search_spider(max_pages):
+    page = 1
+    while page < max_pages :
+        url = "http://nex1music.ir/pages/"+str(page)+"/?s"
+        source_code=requests.get(url)
+        make_text = source_code.text
+        soup = BeautifulSoup(make_text)
+        for link in soup.findAll("a",{"class" : "mre animate"}):
+            href =link.get("href")
+            print(href)
+
+search_spider(2)
+
+
+
